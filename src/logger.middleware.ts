78df00fd9ec7acb7fc2a06495a9db48633f71422
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-11-28 20:16:58 +0100

    added more origins

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -5,7 +5,10 @@ import { NextFunction, Request, Response } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.info('Request....');
-    const allowedOrigins = ['http://localhost:3000'];
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'https://frontend-reactjs-iw8gz4z3b-noobs-discord.vercel.app',
+    ];
     if (allowedOrigins.indexOf(req.header('Origin'))) {
       res.header('Access-Control-Allow-Origin', req.header('Origin'));
       res.header('Access-Control-Allow-Headers', 'content-type');
