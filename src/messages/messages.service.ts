8f0e7494f05f4efa16c3848e2b2d064adbc900da
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-12-25 19:51:58 +0100

    fixed the token and removed all console.log()

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -21,7 +21,6 @@ export class MessagesService {
       !year ? null : date.setFullYear(year);
       const dateStart = new Date(date.setHours(1, 0, 0));
       const dateEnd = new Date(date.setHours(24, 59, 59));
-      console.log(dateStart, dateEnd);
       const users = await this.userModel
         .find({
           'messages.message': {
@@ -44,11 +43,9 @@ export class MessagesService {
   async getWeek(): Promise<any> {
     const date = new Date();
     const week = date.getWeek();
-    console.log(week);
     try {
       const dateStart = new Date(week[0]);
       const dateEnd = new Date(week[1]);
-      console.log(dateStart, dateEnd);
       const users = await this.userModel
         .find({
           'messages.message': {
